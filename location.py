75df9efcc5f63666a4ac71ac7485ee853d139b3e
svsaravanan <30774871+svsaravanan@users.noreply.github.com>
2021-08-26 10:29:43 +0530

    sample location finder
    sample checking a new fiber connection is available or not in your current location

---
+++ location.py
@@ -0,0 +1,9 @@
+print("welcome to jiofiber")
+name = str(input("enter your name: "))
+name = str(input("enter your location: "))
+pincode = int(input("enter your pincode: "))
+if pincode == (int ("600109")):
+    print("we will meet you soon", end='')
+else:
+    print("sorry")
+print()
